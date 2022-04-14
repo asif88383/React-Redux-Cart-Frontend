34bdcc3e81f6fe0e006ce960962fc32230887797
Muhammad Asif <asif88383@gmail.com>
2022-04-14 12:17:04 +0500

    first commit

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios';
 import {openModal} from '../modal/modalSlice';
 
@@ -10,4 +10,66 @@ const initialState = {
     total: 0,
     isLoading: true,
 }
+
+export const getCartItems = createAsyncThunk(
+    'cart/getCartItems',
+    async (name, thunkAPI) => {
+        try {
+            // console.log(name);
+            // console.log(thunkAPI);
+            // console.log(thunkAPI.getState());
+            // thunkAPI.dispatch(openModal());
+            const resp = await axios(url);
+
+            return resp.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue('something went wrong');
+        }
+    }
+);
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        clearCart: (state) => {
+            state.cartItems = [];
+        },
+        removeItem: (state, action) => {
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+        },
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+        },
+        calculateTotals: (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item) => {
+                amount += item.amount;
+                total += item.amount * item.price;
+            });
+            state.amount = amount;
+            state.total = total;
+        },
+    },
+    extraReducers: {
+        [getCartItems.fulfilled]: (state, action) => {
+            state.cartItems = action.payload;
+            state.isLoading = false;
+        },
+        [getCartItems.rejected]: (state) => {
+            console.log(action)
+            state.isLoading = false;
+        },
+        [getCartItems.pending]: (state) => {
+            state.isLoading = true;
+        },
+    },
+});
 
