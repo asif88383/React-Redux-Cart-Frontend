ef0aa1296673677e3103bf39ab96f19490cc50f5
Muhammad Asif <asif88383@gmail.com>
2022-04-14 12:02:17 +0500

    first commit

--- starter/src/index.js
+++ starter/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {store} from './store';
+import {Provider} from 'react-redux';
 
 const rootElement = document.getElementById("root");	
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
